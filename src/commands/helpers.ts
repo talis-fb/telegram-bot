6c086788517e7790b1b2fcb8125e016e21e05b59
talis-fb <talisonf11@gmail.com>
2022-07-31 17:50:18 -0300

    Comando de /help e uso de Views com html

--- src/commands/helpers.ts
+++ src/commands/helpers.ts
@@ -1,9 +1,16 @@
 import { Composer } from 'telegraf'
+import fs from 'fs'
+import path from 'path'
 
 const bot = new Composer()
 
 bot.help(async ctx => {
-  ctx.reply('Digite o valor da compra, com uma linha do local')
+  ctx.replyWithMarkdown(
+    fs.readFileSync(path.join(__dirname, '/views/help.html'), 'utf8'),
+    {
+      parse_mode: 'HTML',
+    }
+  )
 })
 
 export default bot
